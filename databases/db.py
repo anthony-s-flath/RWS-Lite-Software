708f8c66bd134806927277fe068eda955d308aab
anthony <aflath@umich.edu>
2025-08-18 17:03:54 -0400

    disk reading

--- databases/db.py
+++ databases/db.py
@@ -6,6 +6,12 @@ from enum import Enum
 from datetime import datetime
 import pandas as  pd
 import os
+import time
+import requests
+from databases import OnlineDB
+from driver import APP_KEY, APP_SECRET, STATION_NAME
+
+header = "time,in_temp,in_press,in_hum,in_gas,out_temp,out_press,out_hum,out_gas,winddir,windspeed,is_raining,soil_temp,soil_mois,uv,radon,CPM\n"
 
 class Datatype(Enum):
     TIME = 0
@@ -27,16 +33,29 @@ class Datatype(Enum):
     CPM = 16
 
 class Database:
-
-    files_in_mem = 0
-    files_on_disk = 0
-    files_in_cloud = 0
-
     def __init__(self, directory, filename):
+        current_time = time.time()
+
         self.directory = directory
         self.filename = filename
         self.current_data = pd.Series(index=Database.index)
         self.data = pd.DataFrame(index=Database.index)
+        self.data_time = current_time
+        self.num_disk_files = 0
+        self.start_disk_time = current_time
+        self.online_database = OnlineDB(APP_KEY, APP_SECRET, STATION_NAME)
+        
+        # init earliest time in disk
+        for name in os.listdir(self.directory):
+            with open(os.path.join(self.directory, name), 'r') as file:
+                line = file.readline()
+                if (line != header): # not a data file
+                    continue
+
+                for line in file:
+                    time_val = file.readline().split(',')[Datatype.TIME] # gets time value
+                    self.start_disk_time = min(time_val, self.start_disk_time)
+
     
     def change_file(self, new_fname, save_old):
         if (save_old):
@@ -46,7 +65,7 @@ class Database:
 
     def writeCSV(self):
         with open(self.filename, 'w') as file:
-            file.write('time,in_temp,in_press,in_hum,in_gas,out_temp,out_press,out_hum,out_gas,winddir,windspeed,is_raining,soil_temp,soil_mois,uv,radon,CPM\n')
+            file.write(header)
             output = ''
             for row in self.current_data:
                 for elt in row:
@@ -57,6 +76,17 @@ class Database:
                 output += '\n'
 
             file.write(output)
+
+    def upload(self, fname) -> bool:
+        try:
+            self.online_database.upload(fname)
+            self.change_file(f"rws_lite_data{time.time()}.csv", False)
+            return True
+        except requests.exceptions.ConnectionError as ex:
+            self.change_file(f"rws_lite_data{time.time()}.csv", True)
+            print(ex)
+            print("Connection Error to Dropbox")
+            return False
 
     # Returns seconds since epoch
     def convert_time(self, time):
@@ -75,11 +105,11 @@ class Database:
         return True
 
 
-    # TODO: this is slow and doesnt work?
+
     # Pushes currently held data to stored data
     # Sets currently held data to NaN
     def push(self):
-        self.data = pd.concat([self.data, self.current_data.to_frame().T])
+        self.data += self.current_data.to_frame().T
 
         # manipulate in place instead !! not this
         self.current_data = [float("nan") for x in self.current_data]  
@@ -92,13 +122,56 @@ class Database:
         self.start = self.convert_time(time_start)
         self.end = self.convert_time(time_end)
 
-    # Returns table of data requested
-    # Time is the first column, rest of columns are returned in order of parameter types
-    def get(self, time_start, time_end=None, types: list[Datatype] = []) -> pd.DataFrame:
-        self.start = self.convert_time(time_start)
-        self.end = self.convert_time(time_end)
 
-        # return 2d Arr with time v type (if type)
+    # returns Dataframe with data after start from disk and memory
+    # requires to not have to get data from cloud
+    def from_disk(self, start, end, types: list[Datatype] = []) -> pd.DataFrame:
+        df = pd.DataFrame()
+        for i in range(len(self.data)):
+            row = self.data.iloc[i]
+            time_val = row.at[Datatype.TIME]
+            if start <= time_val < end:
+                df += row
+        
+        for name in os.listdir(self.directory):
+            with open(os.path.join(self.directory, name), 'r') as file:
+                line = file.readline()
+                if (line != header): # not a data file
+                    continue
+
+                for line in file:
+                    time_val = file.readline().split(',')[Datatype.TIME] # gets time value
+                    if start <= time_val < end:
+                        df += row
+        
+        df.sort_values("time")
+        return df
+                    
+
+                
+
+    
+
+    # Returns table of data requested
+    # Returns empty table if failed
+    # Time is the first column, rest of columns are returned in order of parameter types
+    def get(self, start, end=None, types: list[Datatype] = []) -> pd.DataFrame:
+        time_now = time.time()
+        start = self.convert_time(start)
+        end = self.convert_time(end)
+        if (start < time_now or start >= end):
+            return pd.DataFrame()
+
+        
+        if (start < self.start_disk_time):
+            print("getting from cloud")
+            #TODO call online db method
+        elif (start < self.data_time):
+            print("getting from disk")
+            return self.from_disk(start, end, types)
+        else:
+           self.data.sort_values("time")
+           return self.data.query(f'time >= {str(str)}')
 
 
 
